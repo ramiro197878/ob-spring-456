73ed0f8aaa3fe6fbfbcfa39b62f5bd40570d3df6
Ramiro Astete <24412705+ramiro197878@users.noreply.github.com>
2022-06-10 21:21:56 +0200

    Ejercicio 4, 5 y 6 curso de spring

--- src/main/java/com/example/ejercicio456/controller/LaptopController.java
+++ src/main/java/com/example/ejercicio456/controller/LaptopController.java
@@ -2,14 +2,17 @@ package com.example.ejercicio456.controller;
 
 import com.example.ejercicio456.entities.Laptop;
 import com.example.ejercicio456.repository.LaptopRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LaptopController {
 
-    private LaptopRepository laptopRepository;
+    private final LaptopRepository laptopRepository;
+
 
     public LaptopController(LaptopRepository laptopRepository) {
         this.laptopRepository = laptopRepository;
@@ -17,13 +20,64 @@ public class LaptopController {
 
 
     @GetMapping("api/laptops")
-    public List<Laptop> findAllLaptop() {
+    public List<Laptop> findAll() {
         return laptopRepository.findAll();
     }
 
+    @GetMapping("api/laptops/{id}")
+    public ResponseEntity<Laptop> findOneById(@PathVariable Long id) {
+
+        Optional<Laptop> laptopopt = laptopRepository.findById(id);
+
+        return laptopopt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
     @PostMapping("api/laptops")
-    public Laptop create(@RequestBody Laptop laptop){
-        return laptopRepository.save(laptop);
+    public ResponseEntity<Laptop> create(@RequestBody Laptop laptop){
+
+        if(laptop.getId() != null) {
+            return ResponseEntity.badRequest().build();
+
+        }
+
+        Laptop result = laptopRepository.save(laptop);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("api/laptops")
+    public ResponseEntity<Laptop> update(@RequestBody Laptop laptop){
+
+        if(laptop.getId() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        if(!laptopRepository.existsById(laptop.getId())){
+            return ResponseEntity.notFound().build();
+        }
+        Laptop result = laptopRepository.save(laptop);
+        return ResponseEntity.ok(result);
+
+    }
+
+    @DeleteMapping("api/laptops/{id}")
+    public ResponseEntity<Laptop> delete(@PathVariable Long id){
+
+        if(!laptopRepository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+
+        laptopRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @DeleteMapping("api/laptops")
+    public ResponseEntity<Laptop> deleteAll(){
+
+        laptopRepository.deleteAll();
+        return ResponseEntity.noContent().build();
+
     }
 
 }
